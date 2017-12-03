4dc614c83c2895c552fbcb985e5e2bf3d82fc1a4
Nate H <nateholechek@gmail.com>
2017-12-03 17:26:21 -0500

    Added in deleted stuff

--- app/src/main/java/com/example/natha/pilltime/MedicineTodayFragment.java
+++ app/src/main/java/com/example/natha/pilltime/MedicineTodayFragment.java
@@ -12,6 +12,7 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Vector;
@@ -151,7 +152,15 @@ public class MedicineTodayFragment extends Fragment {
     }
     public String formatTime(int i){
         String times = "";
-        if (i%100 < 10) {
+        if(i < 60) {
+            if(i < 10) {
+                times += ("00:0"+i + "\n");
+            }
+            else{
+                times += ("00:" + i) + "\n";
+            }
+        }
+        else if (i%100 < 10) {
             times += (i - i % 100) / 100 + ":" + "0" + (i%100) + "\n";
         }
         else{
@@ -165,6 +174,28 @@ public class MedicineTodayFragment extends Fragment {
         int rawTime = hour + minute;
         return rawTime;
     }
+
+    public void sortMedication(ArrayAdapter<String> arrayAdapter){
+        arrayAdapter.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String []arrayO1 = o1.split("\n");
+                String []arrayO2 = o2.split("\n");
+
+                Integer rawO1Time = unFormatTime(arrayO1[2].substring(5));
+                Integer rawO2Time = unFormatTime(arrayO2[2].substring(5));
+
+                if (rawO1Time < rawO2Time) {
+                    return -1;
+                }
+                else{
+                    return 1;
+                }
+
+            }
+        });
+    }
+
  /*   public Vector<String> sortTimes(ArrayL<String> pills){ //pill = taken/nottaken
         int[] times = new int[pills.size()];
         String[] names = new String[pills.size()];
